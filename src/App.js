c465c552beaba084d71b1c2d8ff445c2b5cac508
arunsinghsnd <arunsinghsnd@gmail.com>
2021-05-04 23:12:11 +0530

    inital commit and header implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Home from "./Home";
 
-function App() {
+const App = () => {
+  //I am using BEN naming conversion
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Home />
+      {/* Header */}
+      {/* Banner */}
+      {/* Search */}
+      {/* Card */}
+      {/* Footer */}
+      {/* Search Page */}
+      {/* ..... */}
     </div>
   );
-}
+};
 
 export default App;
