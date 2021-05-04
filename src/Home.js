400f337d120b9ada0e4d0e536418b1cc0ac82381
arunsinghsnd <arunsinghsnd@gmail.com>
2021-05-04 23:58:07 +0530

    working on banner part

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Banner from "./Banner";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>This is Home</h1>
+      <Banner />
     </div>
   );
 };
