8546ddd699767c5313a8bd1a16c083f6bc0ab83a
Arun Singh <arunsinghsnd@gmail.com>
2021-05-25 22:43:33 +0530

    linking the page

--- src/compoenents/Search/Search.js
+++ src/compoenents/Search/Search.js
@@ -8,9 +8,11 @@ import "react-date-range/dist/theme/default.css";
 
 import { Button } from "@material-ui/core";
 import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
 
 //DATE PICKER COMPONENT
 const Search = () => {
+  const history = useHistory();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -32,7 +34,7 @@ const Search = () => {
         <PeopleIcon />
       </h2>
       <input min={0} defaultValue={2} type="number" />
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => history.push("/search")}>Search Airbnb</Button>
     </div>
   );
 };
